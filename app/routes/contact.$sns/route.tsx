85b1b9ab05c6f02373671976d2c3caa4afabccc1
= <=>
2024-03-13 10:35:28 +0100

    Fix /contact redirect

--- app/routes/contact.$sns/route.tsx
+++ app/routes/contact.$sns/route.tsx
@@ -1,21 +1,15 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { useEffect } from "react";
 
 export async function loader({ params }: LoaderFunctionArgs) {
     const links = await import("./sns.json");
     const { sns } = params as { sns: string };
-    return json(links[sns]);
+    const link = links[sns];
+    return link ? redirect(link) : json(link);
 }
 
 export default function Route() {
     const path = useLoaderData<typeof loader>();
-    useEffect(() => {
-        if (path) {
-            console.log(path);
-            window.location.replace(path);
-        }
-    }, [])
     return (
         <div className="flex flex-col items-center justify-center bg-black text-white h-full w-full text-3xl">
             <p>Unknown contact option</p>
